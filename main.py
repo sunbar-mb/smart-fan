e4f1cb9f0231a7ce73dfd4ef26ff8e5109700618
sunbar-mb <74050866+sunbar-mb@users.noreply.github.com>
2020-11-07 20:43:28 +0100

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 # Setup from Smart Home Kit for Smart fan, if the temperature is more than 30, system runs fan for 10 seconds
 # SMART HOME KIT - Temperature sensor external measures in degrees, and is not super stable - measure average value would be better (avg in 10 seconds)
 # Micro:bit - temperature sensor in processor, not so Math.round_with_precision(0, 0)
-
-
 temp = 0
 OLED.init(128, 64)
 serial.redirect_to_usb()
 previous_temp = 0
 
 def on_forever():
-    global temp, previous_temp
-    
-
     if temp > 30:
         music.start_melody(music.built_in_melody(Melodies.BA_DING), MelodyOptions.ONCE)
         pins.digital_write_pin(DigitalPin.P1, 1)
@@ -21,29 +16,22 @@ def on_forever():
         pins.digital_write_pin(DigitalPin.P1, 0)
 basic.forever(on_forever)
 
-
 def temperature_average():
     global temp, previous_temp
-    
     timer_start = input.running_time_micros()
     counter = 0
     temp_counter = 0
-
-    #temp = smarthome.read_temperature(TMP36Type.TMP36_TEMPERATURE_C, AnalogPin.P2)
-    #serial.write_value("x", temp)
-    #serial.write_value("y", input.temperature())
-
-    while (input.running_time_micros() - timer_start) < 5000000
+    # temp = smarthome.read_temperature(TMP36Type.TMP36_TEMPERATURE_C, AnalogPin.P2)
+    # serial.write_value("x", temp)
+    # serial.write_value("y", input.temperature())
+    while input.running_time_micros() - timer_start < 5000000:
         temp_counter += smarthome.read_temperature(TMP36Type.TMP36_TEMPERATURE_C, AnalogPin.P2)
         counter += 1
-    
-    temp = temp_counter / counter
 
+    temp = Math.round(10*(temp_counter / counter))/10
     if temp != previous_temp:
         OLED.clear()
         OLED.write_string("Temperature: ")
         OLED.write_num(temp)
         previous_temp = temp
-
 basic.forever(temperature_average)
-
--- main.ts
+++ main.ts
@@ -6,17 +6,6 @@ OLED.init(128, 64)
 serial.redirectToUSB()
 let previous_temp = 0
 basic.forever(function on_forever() {
-    
-    temp = smarthome.ReadTemperature(TMP36Type.TMP36_temperature_C, AnalogPin.P2)
-    // serial.write_value("x", temp)
-    // serial.write_value("y", input.temperature())
-    if (temp != previous_temp) {
-        OLED.clear()
-        OLED.writeString("Temperature")
-        OLED.writeNum(temp)
-        previous_temp = temp
-    }
-    
     if (temp > 30) {
         music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
         pins.digitalWritePin(DigitalPin.P1, 1)
@@ -30,4 +19,21 @@ basic.forever(function on_forever() {
 basic.forever(function temperature_average() {
     
     let timer_start = input.runningTimeMicros()
+    let counter = 0
+    let temp_counter = 0
+    //  temp = smarthome.read_temperature(TMP36Type.TMP36_TEMPERATURE_C, AnalogPin.P2)
+    //  serial.write_value("x", temp)
+    //  serial.write_value("y", input.temperature())
+    while (input.runningTimeMicros() - timer_start < 5000000) {
+        temp_counter += smarthome.ReadTemperature(TMP36Type.TMP36_temperature_C, AnalogPin.P2)
+        counter += 1
+    }
+    temp = Math.round(10 * (temp_counter / counter)) / 10
+    if (temp != previous_temp) {
+        OLED.clear()
+        OLED.writeString("Temperature: ")
+        OLED.writeNum(temp)
+        previous_temp = temp
+    }
+    
 })
